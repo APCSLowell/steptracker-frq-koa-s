92992085402e6976b656eeb19afeb2161653789d
koa-s <143123833+koa-s@users.noreply.github.com>
2024-04-22 11:42:41 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int min;
+ private int steps;
+ private int numDays;
+ public StepTracker(int n)
+ {
+  min = n;
+  steps=0;
+  numDays = 0;
+ }
+ public void addDailySteps(int n)
+ {
+  steps+=n;
+  numDays++;
+ }
+ public int activeDays()
+ {
+  return numDays;
+ }
+ public double averageSteps()
+ {
+  return (double)steps/numDays;
+ }
 } 
